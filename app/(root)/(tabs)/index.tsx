c270c97c70600c02d9c7f825acb978a51367e7ae
masch <the.masch@gmail.com>
2025-12-27 23:42:02 -0300

    feat: Add image type declarations and update welcome text styling.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
+      <Text className="my-10 font-rubik-bold text-3xl">Welcome to ReState</Text>
 
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
