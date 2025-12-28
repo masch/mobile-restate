c95e78570a8e109838d945363f291dfe2e2e7b66
masch <the.masch@gmail.com>
2025-12-27 22:52:41 -0300

    feat: Integrate NativeWind and Tailwind CSS for styling with updated dependencies.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,10 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 justify-center items-center">
+      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
 
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
